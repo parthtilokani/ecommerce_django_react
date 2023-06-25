99818c7fd964ae19b64e57828d1f11e34ddbea2f
mulamsaurav <mulamsaurav80@gmail.com>
2023-06-25 21:50:36 +0530

    Remove location fetch

--- src/screens/app/Main.jsx
+++ src/screens/app/Main.jsx
@@ -1,40 +1,18 @@
-import {Platform, StyleSheet, AppState, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
+import React, {useState} from 'react';
 import AppHeader from './../../components/AppHeader';
 import Bottomtab from './../../components/BottomTab/Bottomtab.jsx';
 import Home from './BottomTabScreens/Home.jsx';
 import Search from './BottomTabScreens/Search.jsx';
 import Chats from './BottomTabScreens/Chats.jsx';
 import Account from './BottomTabScreens/Account.jsx';
-import {useIsFocused} from '@react-navigation/native';
-import {PERMISSIONS} from 'react-native-permissions';
-import {CheckPermission} from '../../utils/Permission.js';
-import {Getlocation} from '../../utils/Getlocation.js';
-import CustomAlert from '../../components/CustomAlert/CustomAlert.jsx';
 import {appName} from '../../constant/index.js';
 
 const Main = ({navigation}) => {
-  const isFocused = useIsFocused();
   const [tab, setTab] = useState(0);
 
   const getSelectedTab = tabNumber => {
     setTab(tabNumber);
-  };
-
-  useEffect(() => {
-    // setTab(0);
-    getSelectedTab(0);
-    checkPermission();
-  }, [isFocused]);
-
-  const checkPermission = async () => {
-    const permission =
-      Platform.OS === 'ios'
-        ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
-        : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION;
-
-    const isPermissionGranted = await CheckPermission(permission);
-    if (isPermissionGranted) Getlocation();
   };
 
   return (
@@ -61,5 +39,3 @@ const Main = ({navigation}) => {
 };
 
 export default Main;
-
-const styles = StyleSheet.create({});
