ebabb8271c0c6c1fa18409017fccd292051cbfb8
mulamsaurav <mulamsaurav80@gmail.com>
2023-06-28 19:02:10 +0530

    remove unwanted code

--- src/utils/customHook/request.js
+++ src/utils/customHook/request.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-// import Config from 'react-native-config';
 
-export const request = ({url, method, data, headers}) => {
+export const request = async ({url, method, data, headers}) => {
   return axios({
     method: method || 'get',
     url: url,
@@ -12,20 +11,3 @@ export const request = ({url, method, data, headers}) => {
     // },
   });
 };
-
-// export const addAccessTokenToAxios = async token => {
-//   axios.defaults.headers.Authorization = token;
-// };
-
-// const API = Config.API_URL;
-
-// export const axiosSetup = async () => {
-//   const token = await AsyncStorage.getItem('auth_token');
-//   console.log('sadjaksdjaksdha ', token);
-//   axios.defaults.baseURL = API;
-//   axios.defaults.withCredentials = true;
-//   axios.defaults.headers.Authorization = token;
-// };
-
-// import React, {useState, useEffect} from 'react';
-// import axios from 'axios';
