43c30635f05232ced0c2dc03a263ed326120b129
mulamsaurav <mulamsaurav80@gmail.com>
2023-06-28 18:59:18 +0530

    add phone number input for country code

--- src/components/Inputs/Input.jsx
+++ src/components/Inputs/Input.jsx
@@ -1,8 +1,8 @@
 import {
   Image,
+  Platform,
   Pressable,
   StyleSheet,
-  Text,
   TextInput,
   View,
 } from 'react-native';
@@ -17,7 +17,7 @@ import {
   normalize,
   width,
 } from '../../constant';
-
+import PhoneInput from 'react-native-phone-number-input';
 const Input = ({
   id,
   errors,
@@ -32,26 +32,49 @@ const Input = ({
   keyboardType = 'default',
   onFocus,
   showSoftInputOnFocus = true,
+  onChangeCountry,
 }) => {
   const [secure, setSecure] = useState(true);
   return (
     <>
       <View style={[styles.container, SHADOWS.small, style]}>
-        <Image source={leftIcon} style={styles.icon} />
-        <TextInput
-          id={id}
-          placeholder={placeholder}
-          placeholderTextColor={placeholderTextColor}
-          style={styles.input}
-          secureTextEntry={secure && isPassword}
-          autoCapitalize="none"
-          value={value}
-          onChangeText={onChangeText}
-          maxLength={maxLength}
-          keyboardType={keyboardType}
-          onFocus={onFocus}
-          showSoftInputOnFocus={showSoftInputOnFocus}
-        />
+        {id != 'phoneNumber' && <Image source={leftIcon} style={styles.icon} />}
+        {id == 'phoneNumber' ? (
+          <PhoneInput
+            defaultCode="IN"
+            containerStyle={styles.phoneContainer}
+            codeTextStyle={styles.countryCodeTextStyle}
+            flagButtonStyle={styles.phoneFlagBtn}
+            textInputStyle={{alignSelf: 'center'}}
+            textContainerStyle={styles.phoneTextContainer}
+            value={value}
+            textInputProps={{
+              onChangeText: onChangeText,
+              value: value,
+              maxLength: maxLength,
+              placeholder: placeholder,
+              style: styles.phoneInput,
+              keyboardType: keyboardType,
+              placeholderTextColor: 'grey',
+            }}
+            onChangeCountry={onChangeCountry}
+          />
+        ) : (
+          <TextInput
+            id={id}
+            placeholder={placeholder}
+            placeholderTextColor={placeholderTextColor}
+            style={styles.input}
+            secureTextEntry={secure && isPassword}
+            autoCapitalize="none"
+            value={value}
+            onChangeText={onChangeText}
+            maxLength={maxLength}
+            keyboardType={keyboardType}
+            onFocus={onFocus}
+            showSoftInputOnFocus={showSoftInputOnFocus}
+          />
+        )}
         {isPassword && (
           <Pressable
             style={styles.rightIconView}
@@ -62,19 +85,8 @@ const Input = ({
             />
           </Pressable>
         )}
-        {/* {isSearch && (
-        <Pressable style={styles.rightIconView}>
-          <Image source={icons.email} style={styles.icon} />
-        </Pressable>
-      )} */}
       </View>
-      {/* {errors?.find(e => e.id === id) && (
-        <HelperText
-          type="error"
-          style={{fontSize: 14, alignSelf: 'flex-start'}}>
-          {errors.find(e => e.id === id)?.error}
-        </HelperText>
-      )} */}
+
       {errors[id] && (
         <HelperText
           type="error"
@@ -95,7 +107,7 @@ const styles = StyleSheet.create({
     width: width * 0.9,
     height: height * 0.07,
     borderRadius: 10,
-    padding: 3,
+    paddingHorizontal: 5,
     backgroundColor: COLORS.lightWhite,
   },
   icon: {
@@ -103,12 +115,40 @@ const styles = StyleSheet.create({
     height: height * 0.03,
     margin: 5,
     resizeMode: 'contain',
-    // tintColor: COLORS.black,
   },
   input: {
     fontSize: normalize(FONTSIZE.xxSmall),
     width: width * 0.7,
     color: COLORS.black,
+  },
+  phoneInput: {
+    fontSize: normalize(FONTSIZE.xxSmall),
+    width: width * 0.56,
+    color: COLORS.black,
+  },
+  phoneTextContainer: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    color: COLORS.black,
+    paddingVertical: 0,
+  },
+  phoneFlagBtn: {
+    left: 0,
+    borderRadius: 10,
+    width: width * 0.15,
+    height: height * 0.05,
+  },
+  countryCodeTextStyle: {
+    fontSize: normalize(FONTSIZE.small),
+    color: COLORS.black,
+    bottom: Platform.OS == 'android' ? 15 : 0,
+    textAlignVertical: 'center',
+  },
+  phoneContainer: {
+    backgroundColor: COLORS.lightWhite,
+    color: 'black',
+    alignItems: 'center',
+    borderRadius: 10,
   },
   rightIconView: {
     right: 15,
