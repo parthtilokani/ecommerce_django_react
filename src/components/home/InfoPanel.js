30a98e82f1c81bbdf4705e9d2df02b72ffdf1d39
Uddhav Jadhav <jadhavus.bmw@gmail.com>
2023-10-11 20:58:44 +0530

    location based changes

--- src/components/home/InfoPanel.js
+++ src/components/home/InfoPanel.js
@@ -4,82 +4,73 @@ import { useQuery } from "@tanstack/react-query";
 import { axiosOpen } from "../../utils/axios.js";
 
 const InfoPanel = () => {
-  const [fetchedQueries, setFetchedQueries] = useState([false, false, false]);
-  const { data: total_ads } = useQuery({
+  const [fetchedQueries, setFetchedQueries] = useState([false, false]);
+  const { data } = useQuery({
     queryKey: ["total_ads"],
     queryFn: async () => {
       const { data } = await axiosOpen.get("/ads/ads/total_count");
       setFetchedQueries((prev) => [true, prev[1], prev[2]]);
-      return data?.total_count;
+      return {
+        total_count: data?.total_count,
+        distinct_states_count: data?.distinct_states_count,
+      };
     },
     enabled: !fetchedQueries[0],
-  });
-  const { data: total_locations } = useQuery({
-    queryKey: ["total_locations"],
-    queryFn: async () => {
-      const { data } = await axiosOpen.get("/ads/district", {
-        page: 1,
-        page_size: 1000,
-      });
-      setFetchedQueries((prev) => [prev[0], true, prev[2]]);
-      return data?.count || 100;
-    },
-    enabled: !fetchedQueries[1],
   });
   const { data: total_users } = useQuery({
     queryKey: ["total_users"],
     queryFn: async () => {
       const { data } = await axiosOpen.get("/user/user/get_users_count");
-      setFetchedQueries((prev) => [prev[0], prev[1], true]);
+      setFetchedQueries((prev) => [prev[0], true]);
       return data?.registered_user || 100;
     },
-    enabled: !fetchedQueries[2],
+    enabled: !fetchedQueries[1],
   });
 
   return (
-    <div id='home-info-panel'>
-      <div className='info-panel-main'>
-        <div className='info-overlay'>
-          <div className='row mx-auto'>
-            <div className='col-md-4 d-flex justify-content-center align-items-center'>
-              <div className='info-panel-card'>
-                <div className='d-flex flex-fill'>
-                  <div className='ip-img-div'>
-                    <img src='/assets/svgs/ads.svg' alt='' />
+    <div id="home-info-panel">
+      <div className="info-panel-main">
+        <div className="info-overlay">
+          <div className="row mx-auto">
+            <div className="col-md-4 d-flex justify-content-center align-items-center">
+              <div className="info-panel-card">
+                <div className="d-flex flex-fill">
+                  <div className="ip-img-div">
+                    <img src="/assets/svgs/ads.svg" alt="" />
                   </div>
-                  <div className='mt-4'>
-                    <div className='h2 fw-bold text-white'>
-                      {total_ads || 100}+
+                  <div className="mt-4">
+                    <div className="h2 fw-bold text-white">
+                      {data?.total_count || 100}+
                     </div>
-                    <p className='h5 text-white'>Published Ads</p>
+                    <p className="h5 text-white">Published Ads</p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='col-md-4 d-flex justify-content-center align-items-center'>
-              <div className='info-panel-card'>
-                <div className='d-flex flex-fill'>
-                  <div className='ip-img-div'>
-                    <img src='/assets/svgs/users.svg' alt='' />
+            <div className="col-md-4 d-flex justify-content-center align-items-center">
+              <div className="info-panel-card">
+                <div className="d-flex flex-fill">
+                  <div className="ip-img-div">
+                    <img src="/assets/svgs/users.svg" alt="" />
                   </div>
-                  <div className='mt-4'>
-                    <div className='h2 fw-bold text-white'>{total_users}+</div>
-                    <p className='h5 text-white'>Registered Users</p>
+                  <div className="mt-4">
+                    <div className="h2 fw-bold text-white">{total_users}+</div>
+                    <p className="h5 text-white">Registered Users</p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='col-md-4 d-flex justify-content-center align-items-center'>
-              <div className='info-panel-card'>
-                <div className='d-flex flex-fill'>
-                  <div className='ip-img-div'>
-                    <img src='/assets/svgs/verified.svg' alt='' />
+            <div className="col-md-4 d-flex justify-content-center align-items-center">
+              <div className="info-panel-card">
+                <div className="d-flex flex-fill">
+                  <div className="ip-img-div">
+                    <img src="/assets/svgs/verified.svg" alt="" />
                   </div>
-                  <div className='mt-4'>
-                    <div className='h2 fw-bold text-white'>
-                      {total_locations || 100}+
+                  <div className="mt-4">
+                    <div className="h2 fw-bold text-white">
+                      {data?.distinct_states_count || 100}+
                     </div>
-                    <p className='h5 text-white'>Locations</p>
+                    <p className="h5 text-white">Locations</p>
                   </div>
                 </div>
               </div>
--- src/components/home/MostRecentAds.js
+++ src/components/home/MostRecentAds.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import { axiosOpen } from "../../utils/axios.js";
@@ -9,8 +9,7 @@ import useOurLocation from "../../hooks/useOurLocation.js";
 const MostRecentAds = () => {
   const { location: ourLocation } = useOurLocation();
 
-  const [fetchedQueries, setFetchedQueries] = useState(false);
-  const { data: ads } = useQuery({
+  const { data: ads, refetch } = useQuery({
     queryKey: ["most_recent_ads"],
     queryFn: async () => {
       const paramsObj = {
@@ -24,27 +23,30 @@ const MostRecentAds = () => {
       const { data } = await axiosOpen.get("/ads/ads/most_recent", {
         params: paramsObj,
       });
-      setFetchedQueries((prev) => true);
       return data?.popular_category &&
         typeof data?.popular_category === "object"
         ? [...data?.popular_category]
         : [];
     },
-    enabled: !fetchedQueries,
+    enabled: false,
     initialData: [],
   });
 
+  useEffect(() => {
+    refetch();
+  }, [ourLocation]);
+
   return (
-    <div id='home-featured-ads'>
-      <div className='text-center py-4 mx-1'>
-        <div className='h2 fw-bold'>Most Recent Ads</div>
+    <div id="home-featured-ads">
+      <div className="text-center py-4 mx-1">
+        <div className="h2 fw-bold">Most Recent Ads</div>
       </div>
-      <div className='featured-ads-main row mx-auto'>
+      <div className="featured-ads-main row mx-auto">
         {ads?.map((ad, i) => (
-          <div className='col-xl-3 col-lg-4 col-md-6 col-sm-6 p-3' key={i}>
+          <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 p-3" key={i}>
             <Link to={`/ads/view/${ad?.id}`}>
-              <div className='fa-card'>
-                <div className='fa-img-main position-relative'>
+              <div className="fa-card">
+                <div className="fa-img-main position-relative">
                   <img
                     src={
                       ad?.ads_image?.length > 0 &&
@@ -53,15 +55,15 @@ const MostRecentAds = () => {
                         ? ad?.ads_image[0].image
                         : "https://www.radiustheme.com/demo/wordpress/publicdemo/classima/wp-content/themes/classima/assets/img/noimage-listing-thumb.jpg"
                     }
-                    alt=''
+                    alt=""
                   />
-                  <div className='fa-img-overlay'></div>
+                  <div className="fa-img-overlay"></div>
                 </div>
-                <div className='fa-card-body'>
-                  <div className='h6 fw-bold'>{ad?.ad_title}</div>
-                  <p className=''>{ad?.ad_description}</p>
-                  <div className='d-flex align-items-center mt-1'>
-                    <img src='/assets/svgs/time.svg' alt='' />
+                <div className="fa-card-body">
+                  <div className="h6 fw-bold">{ad?.ad_title}</div>
+                  <p className="">{ad?.ad_description}</p>
+                  <div className="d-flex align-items-center mt-1">
+                    <img src="/assets/svgs/time.svg" alt="" />
                     <span>
                       {ad?.posted_on &&
                         new Date(ad?.posted_on).toLocaleString("en-IN", {
@@ -74,12 +76,12 @@ const MostRecentAds = () => {
                         })}
                     </span>
                   </div>
-                  <div className='d-flex align-items-center'>
-                    <img src='/assets/svgs/profile.svg' alt='' />
+                  <div className="d-flex align-items-center">
+                    <img src="/assets/svgs/profile.svg" alt="" />
                     <span>{ad?.create_user?.name}</span>
                   </div>
-                  <div className='d-flex align-items-center'>
-                    <img src='/assets/svgs/location.svg' alt='' />
+                  <div className="d-flex align-items-center">
+                    <img src="/assets/svgs/location.svg" alt="" />
                     <span>{ad?.location || "No Location"}</span>
                   </div>
                 </div>
